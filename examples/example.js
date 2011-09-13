2d2480e262fdf31a0a70f6478f0199f93029b462
Cam Pedersen <diffference@gmail.com>
2011-09-13 11:36:22 -0400

    comply with npm package structure

---
+++ examples/example.js
@@ -0,0 +1,9 @@
+var mesh = require('mesh').connect();
+
+setInterval(function(){
+  mesh.emit('ping', { hello: 'world' });
+}, 2000);
+
+mesh.on('pong', function(data){
+  console.log('Recieved pong!');
+});
---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * Mesh
+ * Cam Pedersen
+ * Sept 8, 2011
+ * Written on a plane from SFO to JFK
+ */
+
+module.exports = require('./lib/mesh.js');
